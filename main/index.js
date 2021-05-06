12b59ddec2567b3d27f8fbf31a1674128aa48100
CallumGilly <43007634+CallumGilly@users.noreply.github.com>
2021-05-06 20:32:53 +0100

    Added unit testing

--- main/index.js
+++ main/index.js
@@ -8,31 +8,29 @@ const port = 3030;
 
 //#region Set up app
 var app = express();
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
 //#endregion
 
 //#region Code to test the JSON parsing mechanisms
+
+//Should return the `User` value inside of testList
 app.get(`/test/jsonOut`, (req, res) => {
     testList = {
-        Users: [
-            {
-                name: `Dave`,
-                time: 2.31,
-                length: 100
-            },
-            {
-                name: `John`,
-                time:4.5,
-                length: 100
-            }
-        ]
+        User: 'David'
     };
-    res.json(testList.Users);
+    res.json({
+        'error': false, 
+        'user': testList.User
+    });
 });
 
+//Should return the data it was sent
 app.post(`/test/bodyParser`, (req,res) => {
     console.log(req.body);
-    res.json(req.body);
+    res.json({
+        'error': false,
+        'data': req.body
+    });
 }); 
 //#endregion
 
---
+++ tests/test.js
@@ -0,0 +1,34 @@
+const supertest = require(`supertest`);
+const should = require(`should`);
+const assert = require('assert');
+
+var server = supertest.agent(`http://localhost:3030`);
+
+describe(`Test Pages unit test:`, () => {
+    it(`Should return the User dave`, (done) => {
+        server
+        .get(`/test/jsonOut`)
+        .expect("Content-type",/json/)
+        .expect(200)
+        .end((err,res) => {
+            res.status.should.equal(200);
+            res.body.error.should.equal(false);
+            res.body.user.should.equal('David');
+            done();
+        });
+    });
+
+    it(`Should return the data send`, () => {
+        const testData = {foo: 'bar'}
+        server
+        .post(`/test/bodyParser`)
+        .send(testData)
+        .expect("Content-type",/json/)
+        .expect(200)
+        .end((err, res) => {
+            res.status.should.equal(200);
+            res.body.error.should.equal(false);
+            res.body.data.foo.should.equal(testData.foo);
+        });
+    });
+});
